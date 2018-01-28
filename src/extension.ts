6c12f46644d2808e8845b9dea04dbca99a5ffa56
Gabriel Duarte <gabrielqduarte@gmail.com>
2018-01-28 19:08:46 -0200

    refactor: removes the workspace root from the default value of the component path

--- src/extension.ts
+++ src/extension.ts
@@ -4,14 +4,14 @@ import {
   getDefaultComponentPath,
   copyPasteComponent,
 } from "copy-paste-component"
+import { join, sep as slash, resolve } from "path"
 
 export function activate(context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     "extension.cpc",
     async (file: vscode.Uri) => {
       const componentPath: string = file.fsPath
-
-      vscode.window.showInformationMessage(file.fsPath)
+      const { rootPath } = vscode.workspace
 
       const newComponentName = await vscode.window.showInputBox({
         ignoreFocusOut: true,
@@ -19,14 +19,23 @@ export function activate(context: vscode.ExtensionContext) {
         prompt: "What is the name of the new component?",
       })
 
+      const defaultComponentPath = getDefaultComponentPath(
+        componentPath,
+        newComponentName,
+      ).replace(rootPath + slash, "")
+
       const newComponentPath = await vscode.window.showInputBox({
         ignoreFocusOut: true,
         placeHolder: "App",
         prompt: "What is the location of the new component?",
-        value: getDefaultComponentPath(componentPath, newComponentName),
+        value: defaultComponentPath,
       })
 
-      copyPasteComponent(componentPath, newComponentName, newComponentPath)
+      await copyPasteComponent(
+        componentPath,
+        newComponentName,
+        join(rootPath, newComponentPath),
+      )
 
       vscode.window.showInformationMessage(
         `Component ${newComponentName} successfully created at ${newComponentPath}`,
